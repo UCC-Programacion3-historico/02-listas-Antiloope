32bfee658796f5041a76bb3bde64ba52c10d75ef
Antiloope <rodrigopizarro1234@gmail.com>
2017-09-04 16:35:21 -0300

    Se completo la funcion invertir y se creo un main de testeo

--- 01_Invierte/fnInvierte.h
+++ 01_Invierte/fnInvierte.h
@@ -2,15 +2,49 @@
 #ifndef FNINVIERTE_H
 #define FNINVIERTE_H
 
-
 template <class T>
 void fnInvierte (Lista<T> *lis);
-
+//Invierte es una funcion recursiva que cambia el orden de lis y lo guarda en inv
+template <class T>
+void invierte(Lista<T> *lis,Lista<T> inv);
+//Copia es una funcion recursiva que copia la lista inv a lis.
+template <class T>
+void copia(Lista<T> *lis,Lista<T> inv);
 
 template <class T>
 void fnInvierte (Lista<T> *lis){
+    if (lis->esVacia()) {
+        return;
+    }
 
+    Lista<T> *inversa = new Lista<T>;
+
+    invierte(lis,inversa);
+    copia(lis,inversa);
 }
 
+template <class T>
+void invierte(Lista<T> *lis,Lista<T> *inv){
+    //Base
+    if (lis->esVacia()) {
+        return;
+    }
+    inv->insertarPrimero(lis->getDato(0));
+    lis->remover(0);
+    //Recursion
+    invierte(lis,inv);
+}
+
+template <class T>
+void copia(Lista<T> *lis,Lista<T> *inv){
+    //Base
+    if (inv->esVacia()){
+        return;
+    }
+    lis->insertarUltimo(inv->getDato(0));
+    inv->remover(0);
+    //Recursion
+    copia(lis,inv);
+}
 
 #endif //FNINVIERTE_H
--- 01_Invierte/main.cpp
+++ 01_Invierte/main.cpp
@@ -5,6 +5,28 @@
 using namespace std;
 
 int main() {
-    
+    Lista<int> lis;
+    int a;
+    do {
+        cout << "Ingrese un elemento a la lista. Si no quiere agregar mas ingrese -1" << '\n';
+        cin >> a;
+        if (a>=0) {
+            lis.insertarUltimo(a);
+        }
+    } while(a>=0);
+
+    cout << "La lista tiene " << lis.getTamanio() << " elementos" << '\n';
+
+    for (int i = 0; i < lis.getTamanio(); i++) {
+        cout << "Elemento numero " << i << ": " << lis.getDato(i) << '\n';
+    }
+
+    fnInvierte(&lis);
+
+    cout << "Ahora la lista esta al reves" << '\n';
+    for (int i = 0; i < lis.getTamanio(); i++) {
+        cout << "Elemento numero " << i << ": " << lis.getDato(i) << '\n';
+    }
+
     return 0;
 }
