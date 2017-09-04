094e8b01cc23ae387e72a726391c3c96226af1d8
Antiloope <rodrigopizarro1234@gmail.com>
2017-09-04 20:00:02 -0300

    Se completo el ejercicio de union usando recursion

--- 04_Union/main.cpp
+++ 04_Union/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include "../Lista/Lista.h"
+#include "union.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 01/04\n" << std::endl;
+    Lista<int> A;
+    Lista<int> B;
+    for (int i = 0; i < 10; i++) {
+        A.insertarPrimero(i);
+    }
+    for (int i = 0; i < 10; i++) {
+        B.insertarUltimo(i);
+    }
+    Lista<int> *R = unir(A,B);
+    for (int i = 0; i < R->getTamanio(); i++) {
+        cout << "Posicion " << i << ": " << R->getDato(i) << '\n';
+    }
     return 0;
 }
--- 04_Union/union.h
+++ 04_Union/union.h
@@ -3,10 +3,26 @@
 #ifndef UNION_H
 #define UNION_H
 
+template<class T>
+void agregar(Lista<T> *lis,Lista<T> *lisA,int tam);
 
 template<class T>
 Lista<T> *unir(Lista<T> &lisA, Lista<T> &lisB) {
-    return new Lista<T>();
+    Lista<T> *lis = new Lista<T>;
+    agregar(lis,&lisB,lisB.getTamanio());
+    agregar(lis,&lisA,lisA.getTamanio());
+    return lis;
+}
+
+template<class T>
+void agregar(Lista<T> *res,Lista<T> *lis,int tam){
+    //Base
+    if (tam == 0){
+        return;
+    }
+    res->insertarPrimero(lis->getDato(tam-1));
+    //Recursion
+    agregar(res,lis,tam-1);
 }
 
 
