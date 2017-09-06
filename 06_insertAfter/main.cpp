e8e46c3a411dba57610083d4e504919b03484c71
Antiloope <rodrigopizarro1234@gmail.com>
2017-09-05 21:30:40 -0300

    Se completo el ejercicio y se realizo un main de testeo

--- 06_insertAfter/main.cpp
+++ 06_insertAfter/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include "../Lista/Lista.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 01/05\n" << std::endl;
+    Lista<int> lis;
+
+    for (int i = 0; i < 10; i++) {
+        lis.insertarPrimero(i);
+    }
+    lis.insertar(3,1);
+    lis.insertar(7,1);
+    lis.insertar(9,1);
+
+    for (int i = 0; i < lis.getTamanio(); i++) {
+        cout << "Posicion " << i << ": " << lis.getDato(i) << '\n';
+    }
+
+    lis.insertAfter2(1,1,45);
+    
+    cout << "\n\n";
+    for (int i = 0; i < lis.getTamanio(); i++) {
+        cout << "Posicion " << i << ": " << lis.getDato(i) << '\n';
+    }
     return 0;
 }
