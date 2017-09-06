4c754a65461ec416b2f437d66d1f9711da67e474
Antiloope <rodrigopizarro1234@gmail.com>
2017-09-06 10:19:30 -0300

    Se impleento la lista circular

--- 05_CircList/CircList.h
+++ 05_CircList/CircList.h
@@ -41,11 +41,12 @@ public:
 
     bool esVacia();
     int getTamanio();
-    void insertarAntes(T dato);
-    void insertarDespues(T dato);
-    T getDato(int pos);
+    void insertar(T dato);
+    void avanzar();
+    T getDato();
     void vaciar();
-
+    void remover();
+    void reemplazar(T dato);
 };
 
 template <class T>
@@ -60,37 +61,89 @@ CircList<T>::CircList(const CircList<T> &li) {
 
 template <class T>
 CircList<T>::~CircList() {
-
+    vaciar();
 }
 
 template <class T>
 bool CircList<T>::esVacia() {
-
+    return puntero == NULL;
 }
 
 template <class T>
 int CircList<T>::getTamanio() {
+    int cont = 1;
+    nodo<T> *aux = puntero;
 
+    if (NULL == puntero) {
+        return 0;
+    }
+    while (aux->getSiguiente() != puntero) {
+        cont++;
+        aux = aux->getSiguiente();
+    }
+    return cont;
 }
 
 template <class T>
-void CircList<T>::insertarAntes(T dato) {
-
+void CircList<T>::insertar(T dato) {
+    if (puntero == NULL) {
+        puntero = new nodo<T>(dato,puntero);
+        return;
+    }
+    nodo<T> *aux = new nodo<T>(dato, puntero->getSiguiente());
+    puntero->setSiguiente(aux);
+    puntero = puntero->getSiguiente();
 }
 
 template <class T>
-void CircList<T>::insertarDespues(T dato) {
-
-}
-
-template <class T>
-T CircList<T>::getDato(int pos) {
-
+T CircList<T>::getDato() {
+    if (puntero == NULL) {
+          throw 1;
+    }
+    return puntero->getDato();
 }
 
 template <class T>
 void CircList<T>::vaciar() {
+    while (!esVacia()) {
+        remover();
+    }
+}
 
+template <class T>
+void CircList<T>::avanzar() {
+    if (puntero != NULL) {
+        puntero = puntero->getSiguiente();
+    }
+    //Si es NULL no hace nada
+}
+
+template <class T>
+void CircList<T>::remover(){
+    nodo<T> *aux = puntero;
+
+    if (puntero == NULL)
+        return;
+
+    while (aux->getSiguiente() != puntero) {
+        aux = aux->getSiguinete();
+    }
+    if (aux == puntero) {
+        delete puntero;
+        puntero = NULL;
+        return;
+    }
+    aux->setSiguiente(puntero->getSiguiente());
+    delete puntero;
+    puntero = aux->getSiguiente();
+}
+
+template <class T>
+void CircList<T>::reemplazar(T dato){
+    if (puntero == NULL) {
+        throw 1;
+    }
+    puntero->setDato(dato);
 }
 
 #endif //CIRCLIST_H
